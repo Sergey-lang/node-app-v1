f7de12cc7a4566ff36c2c03b7ed6dd00628cfad5
Siarhei <siarhei.kukharonak@startmatter.com>
2024-05-19 16:50:17 +0300

    work with router

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const host = '127.0.0.1'
 const port = 8000
 const app = express();
 
+app.use((req, res, next) => {
+    next()
+})
+
 app.get('/hello', (req, res) => {
-    res.send('Hello')
+    res.status(201).json({success: true})
 })
 
-app.post('/hello', (req, res) => {
-    res.send('Hello')
-})
+app.use('/users', userRouter);
 
+app.use((err, res) => {
+    res.status(500).send(err.message)
+})
 app.listen(port, () => {
     console.log(`Start server at ${host}:${port}`)
 })
