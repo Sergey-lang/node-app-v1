081b7faf1e0ea6966321dbc4b65e5f290d9aaa64
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-15 20:55:33 +0300

    e2e tests

---
+++ tests/users.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { boot } from '../src/main';
+import { App } from '../src/app';
+import supertest from 'supertest';
+
+let application: App;
+
+beforeAll(async () => {
+	const { app } = await boot;
+	application = app;
+});
+
+describe('Users e2e', () => {
+	it('Register - error', async () => {
+		const res = await supertest(application.app).post('/users/register').send({
+			email: 'a@mail.ru', password: '1',
+		});
+
+		expect(res.statusCode).toBe(422);
+	});
+
+	it('Login - success', async () => {
+		const res = await supertest(application.app).post('/users/login').send({
+			email: 'www.petya@mail.ru', password: 'asdasd',
+		});
+
+		expect(res.body.jwt).not.toBeUndefined();
+	});
+
+	it('Login - error', async () => {
+		const res = await supertest(application.app).post('/users/login').send({
+			email: 'www.petya@mail.ru', password: '1',
+		});
+
+		expect(res.statusCode).toBe(401);
+	});
+
+	it('Info - success', async () => {
+
+		const login = await supertest(application.app)
+			.post('/users/login')
+			.send({ email: 'www.petya@mail.ru', password: 'asdasd' });
+
+		const res = await supertest(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer ${login.body.jwt}`);
+
+		expect(res.body.email).toBe('www.petya@mail.ru');
+	});
+
+
+	it('Info - error', async () => {
+		const res = await supertest(application.app)
+			.get('/users/info')
+			.set('Authorization', `Bearer 1`);
+
+		expect(res.statusCode).toBe(401);
+	});
+});
+
+afterAll(() => {
+	application.close();
+});
