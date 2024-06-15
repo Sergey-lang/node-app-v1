0a1630cfca7d4f6f7727728dd6c35d5f6d8fc210
Siarhei <siarhei.kukharonak@startmatter.com>
2024-06-15 13:10:57 +0300

    add guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,6 +12,7 @@ import { IUsersService } from './user.service.interface';
 import { ValidateMiddleware } from '../common/validate.middleware';
 import { sign } from 'jsonwebtoken';
 import { IConfigService } from '../config/config.service.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UsersController extends BaseController implements IUsersController {
@@ -38,6 +39,7 @@ export class UsersController extends BaseController implements IUsersController 
 				path: '/info',
 				method: 'get',
 				func: this.info,
+				middlewares: [new AuthGuard()],
 			},
 		]);
 	}
@@ -60,7 +62,8 @@ export class UsersController extends BaseController implements IUsersController 
 	}
 
 	async info({ user }: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): Promise<void> {
-		this.ok(res, { email: user });
+		const userInfo = await this.userService.getUserInfo(user);
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id });
 	}
 
 	private signJWT(email: string, secret: string): Promise<string> {
